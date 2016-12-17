cd8efbaf379c5043fb2f9af2fa95358ec18ee71d
Lactobacillus <cyw314@gmail.com>
2016-12-17 17:12:04 +0900

    debug

--- 100jum_insang.py
+++ 100jum_insang.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -26,4 +27,5 @@ def index():
 
 if __name__ == '__main__':
     
-	app.run()
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host = '0.0.0.0', port = port)
